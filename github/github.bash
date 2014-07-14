9fa998bb99e7802e7dbcba09e24d5bf96246c3a2
Rick Bradley <bradley@github.com>
2014-07-14 16:34:07 -0500

    add trailing bits from old bashrc

---
+++ github/github.bash
@@ -0,0 +1,8 @@
+# Load up The GitHub Setup's environment.
+
+if [ -f "/opt/boxen/env.sh" ]
+  then source "/opt/boxen/env.sh"
+fi
+
+cat ~/.octocat
+# (curl https://api.github.com/octocat > ~/.octocat &) 2> /dev/null
