af2f7bccd01bd14b6513c814bfe1b1d0f1a382b8
Rick Bradley <bradley@github.com>
2014-07-14 15:48:29 -0500

    update for my usage

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,2 @@
 # Pipe my public key to my clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="cat ~/github/ssh/id_github.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
