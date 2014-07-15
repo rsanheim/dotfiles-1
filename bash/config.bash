72eeb9c45f74210afcb459369b857c1db6da4eb1
Rick Bradley <bradley@github.com>
2014-07-14 18:52:14 -0700

    getting back to bash slowly

--- bash/config.bash
+++ bash/config.bash
@@ -1,7 +1,8 @@
-fpath=($BASH/functions $fpath)
-
-autoload -U $BASH/functions/*(:t)
-
 HISTFILE=~/.bash_history
 HISTSIZE=10000
 SAVEHIST=10000
+
+# TODO: fix this!
+#fpath=($BASH/functions $fpath)
+#autoload -U $BASH/functions/*(:t)
+
