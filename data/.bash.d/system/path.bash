fc1a0b9b8d5d07b58f9313756034bae4ee59e5b2
Rick Bradley <bradley@github.com>
2014-07-27 18:25:54 -0500

    improvements to $PATH handling

--- data/.bash.d/system/path.bash
+++ data/.bash.d/system/path.bash
@@ -1,2 +1,8 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$BASH/bin:$PATH"
+# Load up The GitHub Setup's environment.
+
+if [ -f "/opt/boxen/env.sh" ]
+  then source "/opt/boxen/env.sh"
+fi
+
+export PATH="$HOME/bin:./bin:/usr/local/bin:/usr/local/sbin:$BASH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
--- data/.bashrc
+++ data/.bashrc
@@ -31,11 +31,5 @@ do
   source $file
 done
 
-# Load up The GitHub Setup's environment.
-
-if [ -f "/opt/boxen/env.sh" ]
-  then source "/opt/boxen/env.sh"
-fi
-
 cat ~/.octocat
 # (curl https://api.github.com/octocat > ~/.octocat &) 2> /dev/null
