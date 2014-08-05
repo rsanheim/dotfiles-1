257aec7e7742d67b15a79267d199825243b2a57d
Rick Bradley <bradley@github.com>
2014-08-04 20:35:34 -0500

    a couple more aliases

--- data/.bash.d/ruby/aliases.bash
+++ data/.bash.d/ruby/aliases.bash
@@ -1,9 +1,12 @@
 alias sc='script/console'
 alias sg='script/generate'
+alias sb='script/bootstrap'
 alias ss='script/server'
+
 alias rc='rails console'
 alias rg='rails generate'
 alias rs='rails server'
 
 alias bungle='bundle'
 alias blunder='bundle'
+alias bx='bundle exec'
