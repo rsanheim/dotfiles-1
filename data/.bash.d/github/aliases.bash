a386393bb5def9b38f88f264d75e275c578f4cd9
Rick Bradley <bradley@github.com>
2014-08-12 16:17:13 -0500

    alias updates

--- data/.bash.d/github/aliases.bash
+++ data/.bash.d/github/aliases.bash
@@ -1,8 +1,10 @@
 alias gh='cd ~/github/github'
 
+# TODO - let's clean these up to use the bastion host / gh-ssh properly
 alias  garage='ssh -t -v github-staff2-cp1-prd.iad.github.net'
 alias  spider='ssh -t -v github-staff3-cp1-prd.iad.github.net'
 alias machine='ssh -t -v github-staff3-cp1-prd.iad.github.net'
+alias   janky='gh-ssh -t -v janky-fe2-cp1-prd.iad.github.net'
 
 alias gh-ssh='_gh-ssh ""'
 alias gh-console='_gh-ssh gh-console'
