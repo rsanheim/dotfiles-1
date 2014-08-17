f7b672e2a5fe5198df12eeac88a03fc4e17905d4
Rick Bradley <bradley@github.com>
2014-08-17 18:35:28 -0500

    ANDROID_HOME
    also some cleanup of old junk

--- data/.bash.d/bash/config.bash
+++ data/.bash.d/bash/config.bash
@@ -2,7 +2,5 @@ HISTFILE=~/.bash_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-# TODO: fix this!
-#fpath=($BASH/functions $fpath)
-#autoload -U $BASH/functions/*(:t)
-
+# make Android tools available
+export ANDROID_HOME=/opt/boxen/homebrew/opt/android-sdk
