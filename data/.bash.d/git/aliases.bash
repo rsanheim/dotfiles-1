9f17a1106268adfdfed977335364dc8b93c99886
Rick Bradley <bradley@github.com>
2014-09-08 22:07:22 -0500

    alias git-open-commit to 'commit'

--- data/.bash.d/git/aliases.bash
+++ data/.bash.d/git/aliases.bash
@@ -29,4 +29,5 @@ alias pick='git cherry-pick'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gsb='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gt='git-track'
+alias commit='git-open-commit'
 alias ow='git overwritten'
