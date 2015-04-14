c5cff2348710b2f8535c1d73b8f9395b6f9227a1
Rick Bradley <rick@rickbradley.com>
2015-04-14 15:01:59 -0700

    Try to get more fancy with 'hub' detection

--- data/.bash.d/git/aliases.bash
+++ data/.bash.d/git/aliases.bash
@@ -1,6 +1,8 @@
+
+
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-alias git='hub'
+which hub 2>/dev/null | grep hub && alias git='hub'
 
 # git-related aliases
 alias g='git grep'
