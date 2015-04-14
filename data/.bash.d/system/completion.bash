57451598083bb5edc9cc95f52e98877670c92939
Rick Bradley <rick@rickbradley.com>
2015-04-14 17:17:54 -0500

    conditionalize ssh completion on .ssh/config existence

--- data/.bash.d/system/completion.bash
+++ data/.bash.d/system/completion.bash
@@ -1,2 +1,6 @@
 # ssh host completions
-complete -o default -o nospace -W "$(/usr/bin/env ruby -ne 'puts $_.split(/[,\s]+/)[1..-1].reject{|host| host.match(/\*|\?/)} if $_.match(/^\s*Host\s+/);' < $HOME/.ssh/config)" scp sftp ssh rsync
+
+if [ -f "${HOME}/.ssh/config" ]
+then
+  complete -o default -o nospace -W "$(/usr/bin/env ruby -ne 'puts $_.split(/[,\s]+/)[1..-1].reject{|host| host.match(/\*|\?/)} if $_.match(/^\s*Host\s+/);' < $HOME/.ssh/config)" scp sftp ssh rsync
+fi
